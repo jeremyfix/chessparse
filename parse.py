6d752be0d630e8a4934cc02d8d277fb1b3b1c70c
Jerem <jeremy.fix@centralesupelec.fr>
2020-03-25 22:50:36 +0100

    upd: there is something wrong with the hough

--- parse.py
+++ parse.py
@@ -9,6 +9,9 @@ import skimage.color
 import skimage.feature
 import cv2
 
+
+logging.basicConfig(level=logging.DEBUG)
+
 if len(sys.argv) != 2:
     logging.error("Usage : {} imagefile".format(sys.argv[0]))
     sys.exit(-1)
@@ -16,26 +19,45 @@ if len(sys.argv) != 2:
 
 # Load the image
 imagefilepath = sys.argv[1]
-logging.info("Opening ", imagefilepath)
-rgb_image = skimage.io.imread(imagefilepath)
+logging.info("Opening {}".format(imagefilepath))
 
 # Convert it to grayscale
 rgb_image = cv2.imread(imagefilepath)
 gray_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
 
-# Extract the edges
-edges = cv2.Canny(gray_image, 50, 150, apertureSize=3)
-cv2.imwrite('edges-50-150.jpg', edges)
+# Binarize the image to find the grid 
+ngray = cv2.bitwise_not(gray_image)
+# threshold the image, setting all foreground pixels to
+# 255 and all background pixels to 0
+thresh = cv2.threshold(gray_image, 0, 255,
+                        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+cv2.imwrite('thres.jpg', thresh)
+
+# lines = cv2.HoughLines(thresh,1,np.pi/180,850)
+# logging.info("{} lines detected".format(lines.shape[0]))
+# for rho,theta in lines.squeeze():
+# 	a = np.cos(theta)
+# 	b = np.sin(theta)
+# 	x0 = a*rho
+# 	y0 = b*rho
+# 	x1 = int(x0 + 1000*(-b))
+# 	y1 = int(y0 + 1000*(a))
+# 	x2 = int(x0 - 1000*(-b))
+# 	y2 = int(y0 - 1000*(a))
+
+# 	cv2.line(rgb_image,(x1,y1),(x2,y2),(0,0,255),2)
 
 # From which we extract lines
-minLineLength = 100
-maxLineGap = 30
-lines = cv2.HoughLinesP(edges, 0.1,
-                        np.pi/2000,
-                        30,
-                        minLineLength,maxLineGap)
-for l in lines:
-    x1,y1,x2,y2 = l[0]
-    cv2.line(rgb_image, (x1,y1),(x2,y2),(0,255,0),2)
+minLineLength = 30
+maxLineGap = 10
+lines = cv2.HoughLinesP(thresh, 1,
+                        np.pi/180,
+                        80,
+                        minLineLength=minLineLength,
+						maxLineGap=maxLineGap)
+logging.info("Got {} lines".format(len(lines)))
+for x1,y1,x2,y2 in lines.squeeze():
+	# print(np.sqrt((x2-x1)**2 + (y2-y1)**2))
+	cv2.line(rgb_image, (x1,y1),(x2,y2),(0,255,0),2)
 
 cv2.imwrite('houghlines.jpg', rgb_image)
