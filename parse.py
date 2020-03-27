f3161b73e894932c16cb78129739b093fc7b8618
Jerem <jeremy.fix@centralesupelec.fr>
2020-03-27 17:37:53 +0100

    The grid is correctly identified

--- parse.py
+++ parse.py
@@ -30,48 +30,48 @@ logging.info("Opening {}".format(imagefilepath))
 
 # Convert it to grayscale
 rgb_image = cv2.imread(imagefilepath)
+result_image = rgb_image.copy()
 gray_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
 
 # Little blur
 blur_image = cv2.GaussianBlur(gray_image,(7, 7),0)
-cv2.imwrite('blur.jpg', blur_image)
+cv2.imwrite('0-blur.jpg', blur_image)
 # thres_image = cv2.threshold(blur_image, 0, 255,
 # cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 thres_image = cv2.adaptiveThreshold(blur_image, 255,
                                     cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                     cv2.THRESH_BINARY, 7, 5)
 
+cv2.imwrite('1-threshold.jpg', thres_image)
 # Binarize the image to find the grid 
 ngray = cv2.bitwise_not(thres_image)
-# threshold the image, setting all foreground pixels to
-# 255 and all background pixels to 0
-cv2.imwrite('thres.jpg', thres_image)
+cv2.imwrite('2-not.jpg', ngray)
 
-# lines = cv2.HoughLines(thresh,1,np.pi/180,850)
-# logging.info("{} lines detected".format(lines.shape[0]))
-# for rho,theta in lines.squeeze():
-# 	a = np.cos(theta)
-# 	b = np.sin(theta)
-# 	x0 = a*rho
-# 	y0 = b*rho
-# 	x1 = int(x0 + 1000*(-b))
-# 	y1 = int(y0 + 1000*(a))
-# 	x2 = int(x0 - 1000*(-b))
-# 	y2 = int(y0 - 1000*(a))
+# Dilation to fill in some gaps
+kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+dilation = cv2.dilate(ngray, kernel, iterations=1)
+cv2.imwrite('3-dilation.jpg', dilation)
 
-# 	cv2.line(rgb_image,(x1,y1),(x2,y2),(0,0,255),2)
+# Detect the connected components
+retval, labels, stats, centroids = cv2.connectedComponentsWithStats(dilation, connectivity=4)
 
-# From which we extract lines
-minLineLength = 30
-maxLineGap = 10
-lines = cv2.HoughLinesP(thresh, 1,
-                        np.pi/180,
-                        80,
-                        minLineLength=minLineLength,
-						maxLineGap=maxLineGap)
-logging.info("Got {} lines".format(len(lines)))
-for x1,y1,x2,y2 in lines.squeeze():
-	# print(np.sqrt((x2-x1)**2 + (y2-y1)**2))
-	cv2.line(rgb_image, (x1,y1),(x2,y2),(0,255,0),2)
+# It seems the index 0 is the whole image
+largest_cc_idx=np.argmax(stats[1:, cv2.CC_STAT_AREA])+1
+logging.info("The largest component I found is idx {} with area {}".format(largest_cc_idx, stats[largest_cc_idx, cv2.CC_STAT_AREA]))
 
-cv2.imwrite('houghlines.jpg', rgb_image)
+# Store the top left pixel and with , height of the grid
+coordinates_grid = {"left": stats[largest_cc_idx, cv2.CC_STAT_LEFT],
+                    "top": stats[largest_cc_idx, cv2.CC_STAT_TOP],
+                    "width": stats[largest_cc_idx, cv2.CC_STAT_WIDTH],
+                    "height": stats[largest_cc_idx, cv2.CC_STAT_HEIGHT]
+                   }
+logging.info("BBox of the largest object {}".format(coordinates_grid))
+
+cv2.rectangle(result_image, (0, 0), (50, 50), color=(255, 0, 0), thickness=3)
+cv2.rectangle(result_image,
+              (coordinates_grid["left"], coordinates_grid["top"]),
+              (coordinates_grid["left"] + coordinates_grid["width"],
+               coordinates_grid["top"] + coordinates_grid["height"]),
+              color=(255, 0, 0), thickness=2)
+cv2.imwrite("4-grid.jpg", result_image)
+
